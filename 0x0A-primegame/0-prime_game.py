b1c3a7eff8c02b88056cead1490bc6b7cac9d3e6
Greytiger23 <isabelmothoa49@gmail.com>
2024-09-04 18:56:37 -0700

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,40 +2,30 @@
 """ Function that determines winner of the game based on different values of n
     then returns the player with the most wins. """
 
+
 def isWinner(x, nums):
     """ determines winner of the game and shows result """
-    if not nums or x <= 0:
+    if not nums or x < 1:
         return None
 
-    def prime_game(n):
-        is_prime = [True] * (n + 1)
-        p = 2
-        while p * p <= n:
-            if is_prime[p]:
-                for i in range(p * p, n + 1, p):
-                    is_prime[i] = False
-            p += 1
-        is_prime[0], is_prime[1] = False, False
-        return [i for i in range(n + 1) if is_prime[i]]
+    max_num = max(nums)
+
+    primes = [True] * (max_num + 1)
+    primes[0] = primes[1] = False
+    for i in range(2, int(max_num ** 0.5) + 1):
+        if primes[i]:
+            for multiple in range(i * i, max_num + 1, i):
+                primes[multiple] = False
+
+    prime_count = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if primes[i] else 0)
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        primes = prime_game(n)
-        if not primes:
-            ben_wins += 1
-            continue
-
-        moves = 0
-        for prime in primes:
-            moves += 1
-            multiples = list(range(prime, n + 1, prime))
-            for m in multiples:
-                if m in primes:
-                    primes.remove(m)
-
-        if moves % 2 != 0:
+        if prime_count[n] % 2 != 0:
             maria_wins += 1
         else:
             ben_wins += 1
